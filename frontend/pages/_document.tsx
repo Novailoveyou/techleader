0c0ee789a7645d685e4bdcf299de91e5e6eb8f40
novailoveyou <novailoveyoux@gmail.com>
2022-05-13 09:08:34 +0300

    added fonts

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -5,8 +5,8 @@ import Document, {
   NextScript,
   DocumentContext
 } from 'next/document'
-import { themeColor, company, prod, gtm } from '@/config/index'
-import { MetaIcons, MetaOg, MetaTwitter } from '@/components/meta'
+// import { themeColor, company, prod, gtm } from '@/config/index'
+// import { MetaIcons, MetaOg, MetaTwitter } from '@/components/meta'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -19,38 +19,37 @@ class MyDocument extends Document {
       <Html>
         <Head>
           {/* <meta charSet='UTF-8' /> */}
-          <link href='https://fonts.googleapis.com' />
+
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          {/* @ts-expect-error */}
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
           <link
-            rel='preconnect'
-            href='https://fonts.gstatic.com' /* crossOrigin */
-          />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Arimo:wght@400;500;600;700&display=swap'
-            rel='stylesheet'
-          />
-          <meta name='application-name' content={company.name} />
+            href='https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Raleway:wght@400;600&display=swap'
+            rel='stylesheet'></link>
+
+          {/* <meta name='application-name' content={company.name} /> */}
           <meta name='apple-mobile-web-app-capable' content='yes' />
           <meta
             name='apple-mobile-web-app-status-bar-style'
             content='default'
           />
-          <meta name='apple-mobile-web-app-title' content={company.name} />
+          {/* <meta name='apple-mobile-web-app-title' content={company.name} /> */}
           {/* <meta name='format-detection' content='telephone=no' /> */}
           <meta name='mobile-web-app-capable' content='yes' />
           {/* <meta
         name='msapplication-config'
         content='/assets/imgs/icons/browserconfig.xml'
       /> */}
-          <meta name='msapplication-TileColor' content={themeColor} />
+          {/* <meta name='msapplication-TileColor' content={themeColor} /> */}
           <meta name='msapplication-tap-highlight' content='no' />
-          <meta name='theme-color' content={themeColor} />
+          {/* <meta name='theme-color' content={themeColor} /> */}
 
-          <MetaIcons />
+          {/* <MetaIcons /> */}
 
           <link rel='manifest' href='/manifest.json' />
 
-          <MetaTwitter />
-          <MetaOg />
+          {/* <MetaTwitter /> */}
+          {/* <MetaOg /> */}
         </Head>
         <body>
           {/* {prod && (
