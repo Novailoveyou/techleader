e06f8f71908f2976e2b813bca6d04ce5ffdadee2
novailoveyou <novailoveyoux@gmail.com>
2022-06-05 14:14:53 +0300

    added animation to GeneralProgressLine

--- frontend/components/general/GeneralProgressLine.tsx
+++ frontend/components/general/GeneralProgressLine.tsx
@@ -1,10 +1,10 @@
 import stls from '@/styles/components/general/GeneralProgressLine.module.sass'
 import { TGeneralColorHex, TPropClassNames } from '@/types/index'
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import cn from 'classnames'
 import { selectors } from '@/config/index'
 import { getClassNames } from '@/helpers/index'
-import { useCompanyInfo } from '@/hooks/index'
+import { useNavScrollDir } from '@/hooks/index'
 import { GeneralNonBrakingSpace } from '@/components/general'
 
 type TGeneralProgressLineProps = TPropClassNames & {
@@ -23,6 +23,29 @@ const GeneralProgressLine: FC<TGeneralProgressLineProps> = ({
   containerBgc,
   progressLineBgc
 }) => {
+  const scrollDir = useNavScrollDir()
+
+  const targetWidth = (100 / (listLength - 1)) * curListItemIdx
+  const initialWidth =
+    scrollDir === 'top'
+      ? targetWidth + 100 / (listLength - 1)
+      : targetWidth - 100 / (listLength - 1)
+
+  // if (isProgressivelyFilling) {
+  //   console.log('targetWidth: ', targetWidth)
+  //   console.log('initialWidth: ', initialWidth)
+  // }
+
+  const [
+    progressLineProgressivelFillingWidth,
+    setProgressLineProgressivelFillingWidth
+  ] = useState(initialWidth)
+
+  useEffect(() => {
+    setProgressLineProgressivelFillingWidth(initialWidth)
+    setProgressLineProgressivelFillingWidth(targetWidth)
+  }, [initialWidth, targetWidth, setProgressLineProgressivelFillingWidth])
+
   return (
     <span
       className={cn(stls.container, getClassNames({ classNames }), {
@@ -35,7 +58,7 @@ const GeneralProgressLine: FC<TGeneralProgressLineProps> = ({
         })}
         style={{
           width: isProgressivelyFilling
-            ? `${(100 / (listLength - 1)) * curListItemIdx}%`
+            ? `${progressLineProgressivelFillingWidth}%`
             : `${100 / listLength}%`,
           marginLeft: !isProgressivelyFilling
             ? `${(100 / listLength) * curListItemIdx}%`
